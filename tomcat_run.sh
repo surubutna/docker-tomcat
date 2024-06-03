a1bcb598a3276489f603dabbddcf648b4b41d384
Frederico Silva <surubutna@gmail.com>
2024-06-03 11:11:38 +0100

    Update tomcat_run.sh

--- tomcat_run.sh
+++ tomcat_run.sh
@@ -15,24 +15,24 @@ IMAGE_NAME="tomcat-ssl3"
 CONTAINER_NAME="tomcat-ssl3-container"
 
 #Build image
-echo -e "\n${BLUE}Building the Docker image...${NC}\n"
+echo -e "\n${BLUE}Building $IMAGE_NAME image...${NC}\n"
 docker build -t $IMAGE_NAME .
 
 #Check build
 if [ $? -ne 0 ]; then
-  echo -e"\n${RED}ERROR: Failed to build the Docker image.${NC}\n"
+  echo -e"\n${RED}ERROR: Failed to build $IMAGE_NAME image.${NC}\n"
   exit 1
 fi
 
 #Run container
-echo -e "\n${BLUE}Running the Docker container...${NC}\n"
+echo -e "\n${BLUE}Running $CONTAINER_NAME...${NC}\n"
 docker run -d -p 4041:4041 --name $CONTAINER_NAME $IMAGE_NAME
 
 #Check run
 if [ $? -ne 0 ]; then
-  echo -e "\n${RED}Failed to start the Docker container.${NC}\n"
+  echo -e "\n${RED}Failed to start $CONTAINER_NAME container.${NC}\n"
   exit 1
 fi
 
 #A-Ok
-echo -e "\n${GREEN}Docker container is running successfully.${NC}\n"
+echo -e "\n${GREEN}$CONTAINER_NAME is running successfully.${NC} \n\nAccess app at https://localhost:4041/sample \n"
